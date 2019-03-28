7e9406277b432ab989ad807a1a3f8276533e05f3
imwng <48729383+imwng@users.noreply.github.com>
2019-03-28 15:43:34 +0800

    Add files via upload

---
+++ games/2048/customArray.js
@@ -0,0 +1,45 @@
+/**
+ * 二维数组的转置函数
+ * @return 无返回，作用于原数组
+ */ 
+Array.prototype.transpose = function () {
+  if (this.length <= 0 || !this[0].length) return this;
+  let temp = this[0].map((col, i) => {
+    return this.map(row => {
+      return row[i];
+    });
+  });
+  temp.forEach((item, index) => {
+    this[index] = item;
+  });
+  this.length = temp.length;
+}
+
+/**
+ * 清空数组里的0元素
+ * @return 无返回，作用于原数组
+ */
+Array.prototype.clearZero = function () {
+  let temp = this.filter(item => {
+    return item != 0;
+  });
+  temp.forEach((item, index) => {
+    this[index] = item;
+  });
+  this.length = temp.length;
+}
+
+/**
+ * 一维数组补0
+ * @return 无返回，作用于原数组
+ */
+Array.prototype.paddingZero = function (length, reverse = false) {
+  if (length < this.length) return;
+  let minus = length - this.length;
+  let padding = ('0').repeat(minus).split('');
+  let temp = reverse ? padding.concat(this) : this.concat(padding);
+  temp.forEach((item, index) => {
+    this[index] = item;
+  });
+  this.length = temp.length;
+}
